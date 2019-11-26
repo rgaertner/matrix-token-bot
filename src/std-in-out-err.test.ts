20f81605e239cfa8c6c43f05cc81a6278e4c1a9f
Meno Abels <meno.abels@adviser.com>
2019-11-26 17:28:33 +0100

    wip

--- src/std-in-out-err.test.ts
+++ src/std-in-out-err.test.ts
@@ -1,7 +1,94 @@
-test("connect stream to rx", () => {
+import { StdInOutErr } from "./std-in-out-err";
+
+import { stdin as Stdin } from 'mock-stdin';
+
+import { ObjectReadableMock, ObjectWritableMock } from 'stream-mock'
+import { Subject } from 'rxjs';
+
+test("connect constructor and close", () => {
+  const stdin = new ObjectReadableMock([]);
+  const stdout = new ObjectWritableMock();
+  const stderr = new ObjectWritableMock();
   const stdioe = new StdInOutErr({
-    stdin: process.stdin,
-    stdout: process.stdout,
-    stderr: process.stderr
+    stdin,
+    stdout,
+    stderr
   });
+  expect(stdioe).toBeTruthy();
+  expect(stdioe.stdin.destroyed).toBeFalsy();
+  stdioe.stop();
+  expect(stdioe.stdin.destroyed).toBeTruthy();
+});
+
+test("test stdin", done => {
+  const stdin = new ObjectReadableMock([]);
+  const stdout = new ObjectWritableMock();
+  const stderr = new ObjectWritableMock();
+  const stdioe = new StdInOutErr({
+    stdin,
+    stdout,
+    stderr,
+  });
+  const tst = ['hallo\n', 'data\n'];
+  let seq = 0;
+  stdioe.emitIn.subscribe(msg => {
+    try {
+      expect(msg.toString()).toBe(tst[seq++]);
+      if (seq >= tst.length) {
+        done();
+      }
+    } catch (e) {
+      done(e);
+    }
+  });
+  tst.forEach(i => stdin.emit('data', Buffer.from(i)));
+});
+
+interface PreFactory {
+  readonly stdin: ObjectReadableMock;
+  readonly stdout: ObjectWritableMock;
+  readonly stderr: ObjectWritableMock;
+  readonly stdioe: StdInOutErr;
+}
+
+interface PostFactory extends PreFactory {
+  readonly sout: ObjectWritableMock;
+  readonly subOut: Subject<Buffer>;
+}
+
+test("test stdout/stderr", async () => {
+  const factory = (cb: (p: PreFactory) => PostFactory) => {
+    const stdin = new ObjectReadableMock([]);
+    const stdout = new ObjectWritableMock();
+    const stderr = new ObjectWritableMock();
+    const stdioe = new StdInOutErr({
+      stdin,
+      stdout,
+      stderr,
+    });
+    return cb({
+      stdin, stdout, stderr, stdioe
+    });
+  };
+  return Promise.all([factory(o => ({
+    ...o,
+    subOut: o.stdioe.recvOut,
+    sout: o.stdout
+  })),
+  factory(o => ({
+    ...o,
+    subOut: o.stdioe.recvErr,
+    sout: o.stderr
+  }))].map(async (o) => {
+    let finischedRs;
+    const finisched = new Promise(rs => finischedRs = rs);
+    o.sout.on('finish', finischedRs);
+    const tst = ['hallo\n', 'data\n'].map(i => Buffer.from(i));
+    tst.forEach(i => o.subOut.next(i));
+    expect(o.sout.data).toEqual(tst);
+    o.stdioe.stop();
+    tst.forEach(i => o.subOut.next(Buffer.from(i)));
+    expect(o.sout.data).toEqual(tst);
+    await finisched;
+  }));
 });
--- src/std-in-out-err.ts
+++ src/std-in-out-err.ts
@@ -1,18 +1,57 @@
-import { Stream } from "stream";
+import * as stream from "stream";
+import * as rx from 'rxjs';
+import { WriteStream } from 'fs';
+import { Socket } from 'dgram';
+import { Subject, Subscriber } from 'rxjs';
 
-interface StdInOutErrProps {
-  stdin: Stream;
-  stdout: Stream;
-  stderr: Stream;
+export interface StdInOutErrProps {
+  readonly stdin: stream.Readable;
+  readonly stdout: stream.Writable;
+  readonly stderr: stream.Writable;
 }
 
-class StdInOutErr {
-  private stdin: Stream;
-  private stdout: Stream;
+export class StdInOutErr {
+  public readonly stdin: stream.Readable;
+  public readonly stdout: stream.Writable;
+  public readonly stderr: stream.Writable;
 
-  private stder: Stream;
+  private readonly stdinSub: rx.Subscription;
+
+  public readonly emitIn = new Subject<Buffer>();
+  public readonly recvOut = new Subject<Buffer>();
+  public readonly recvErr = new Subject<Buffer>();
+
+  private readonly subRecvOut: rx.Subscription;
+  private readonly subRecvErr: rx.Subscription;
+
+  private stdinListener = (buf: Buffer) => {
+    // console.log(`XXXXX`, buf);
+    this.emitIn.next(buf);
+  }
 
   constructor(props: StdInOutErrProps) {
-    const { stdin, stdout, stderr } = props;
+    this.stdin = props.stdin;
+    this.stdout = props.stdout;
+    this.stderr = props.stderr;
+    // this.stdin.pipe(this.stdout);
+    this.stdin.on('data', this.stdinListener);
+    this.subRecvOut = this.recvOut.subscribe(msg => {
+      // console.log('Write Out', msg);
+      this.stdout.write(msg);
+    });
+    this.subRecvErr = this.recvErr.subscribe(msg => {
+      // console.log('Write Err', msg);
+      this.stderr.write(msg);
+    })
   }
+
+  public stop() {
+    // console.log('STOP');
+    this.subRecvOut.unsubscribe();
+    this.subRecvErr.unsubscribe();
+    this.stdout.end();
+    this.stderr.end();
+    this.stdin.removeListener('data', this.stdinListener).destroy();
+  }
+
 }
